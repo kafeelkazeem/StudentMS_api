b208458139a653f2e1da8bd253038b07922a7168
kafeelkazeem <kafeelkazeem@gmail.com>
2024-08-05 14:01:47 +0100

    new changes

--- models/admin.js
+++ models/admin.js
@@ -21,7 +21,6 @@ const AdminSchema = new Schema({
     }
 }, {timestamps: true})
   
-  // Method to verify password during login
   AdminSchema.pre('save', async function (next) {
     if (this.isModified('password')) {
       this.password = await bcrypt.hash(this.password, 10);
